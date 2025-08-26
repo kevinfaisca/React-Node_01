d96626d0c94c917bbd1973b75fbe995a87339678
kevinfaisca <kmlfaisca@gmail.com>
2025-08-26 08:38:07 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Estudar React'
+  ]);
 
-  const [user, setUser] = useState({});
+
+  useEffect(() =>{
+    const tarefasStorage = localStorage.getItem('@tarefa')
+    if(tarefasStorage){
+        setTarefas(JSON.parse(tarefasStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefa', JSON.stringify(tarefas));
+
+  },[tarefas]);
 
   function registro(e){
     e.preventDefault();
 
-    setUser({
-      nome: nome,
-      email: email,
-      idade: idade
-    })
+    setTarefas([...tarefas, input])
+    setInput('');
   }
 
   return (
     <div>
       <form onSubmit={registro}>
-        <label>Nome:</label><br/>
-        <input placeholder='Digite seu nome' value={nome} onChange={(e) =>{setNome(e.target.value)}} /><br/>
-
-        <label>Email:</label><br/>
-        <input placeholder='Digite seu email' value={email}  onChange={(e) =>{setEmail(e.target.value)}}/><br/>
-
-        <label>Idade:</label><br/>
-        <input placeholder='Digite seu idade' value={idade}  onChange={(e) =>{setIdade(e.target.value)}}/><br/>
-
+        <label>Nome da tarefa:</label><br/>
+        <input placeholder='Digite uma tarefa' value={input} onChange={(e) =>{setInput(e.target.value)}} /><br/>
         <button type="submit">Salvar</button>
       </form>
       <br/><br/>
-      <div>
-        <span>Nome: {user.nome}</span><br/>
-        <span>Email: {user.email}</span><br/>
-        <span>Idade: {user.idade}</span><br/>
-      </div>
+      <ul>
+        {tarefas.map( tarefa =>(
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
